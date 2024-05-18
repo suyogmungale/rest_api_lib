344dc4313d5f1172a4291ed98af163efff39b9a1
suyogmungale <suyogmungale5@gmail.com>
2024-05-19 00:08:07 +0530

    basic setup is done

--- server.ts
+++ server.ts
@@ -1,5 +1,11 @@
-import app from './src/app.ts';
+import app from './src/app';
 
 const startServer = () => {
-    
+    const port = process.env.port || 3000
+
+    app.listen(port, () => {
+        console.log("listening on port 3000")
+    })
 }
+
+startServer();
