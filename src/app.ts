d6f9a47c1849ea90ae3132018a94902b505a5063
suyogmungale <suyogmungale5@gmail.com>
2024-06-01 10:59:22 +0530

    error handlers added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import express  from "express";
-
+import { error } from "console";
+import express , {NextFunction,Request,Response} from "express";
+import { HttpError } from "http-errors";
 const app = express();
 
-app.get("/",(req,res,nex) => {
+app.get("/",(req,res,next) => {
     res.json({"mesaage":"welcome here"})
 })
+
+app.use((err:HttpError,req:Request,res:Response,next:NextFunction) =>{
+    const statusCode = err.statusCode || 500;
+
+
+    return res.status(statusCode).json({
+        message:err.message,
+        errorStack:err.stack
+    })
+});
 
 export default app;
